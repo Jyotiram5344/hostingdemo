7f50b6f6016d584a16d5bc7d2e86f68d40b71700
Jyotiram5344 <150427962+Jyotiram5344@users.noreply.github.com>
2025-06-15 08:30:47 +0530

    Update db.js

--- backend/server/db.js
+++ backend/server/db.js
@@ -1,16 +1,17 @@
-const mysql = require('mysql2');
 require('dotenv').config();
+const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log('MySQL connected');
+  console.log("MySQL connected!");
 });
 
 module.exports = connection;
